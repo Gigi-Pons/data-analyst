bf4e54b8bed032d7b1713a09cba7e851f2403ac6
Grecia Bueno <77705265+Gigi-Pons@users.noreply.github.com>
2025-08-05 14:31:57 -0600

    Update 01-select-basics.sql

--- sql/01-select-basics.sql
+++ sql/01-select-basics.sql
@@ -1,24 +1,24 @@
--- List all employee information sorted by gender and then by age in descending order
+-- 1. List all employee information sorted by gender and then by age in descending order
 SELECT * 
 FROM employee_information
 ORDER BY gender, age DESC;
 
--- Sort the result by birth date from oldest to youngest
+-- 2. Sort the result by birth date from oldest to youngest
 SELECT * 
 FROM employee_information 
 ORDER BY birth_date ASC;
 
--- Get salary details for employees in department 3
+-- 3. Get salary details for employees in department 3
 SELECT * 
 FROM employee_salary
 WHERE dept_id = 3;
 
--- Only shows rows where the occupation is either 'Data Analyst' or 'IT Specialist'
+-- 4. Only shows rows where the occupation is either 'Data Analyst' or 'IT Specialist'
 SELECT * 
 FROM employee_salary
 WHERE occupation IN ('Data Analyst', 'IT Specialist');
 
--- Find employees born between 1985 and 1995
+-- 5. Find employees born between 1985 and 1995
 SELECT first_name, last_name, birth_date
 FROM employee_information
 WHERE birth_date BETWEEN '1985-01-01' AND '1995-01-01';
